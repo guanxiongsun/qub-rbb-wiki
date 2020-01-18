61827a947610a6aaaad24e2fff07035faca7e9b6
sunguanxiong <sungx.cs@gmail.com>
2020-01-18 16:40:09 +0000

    add rtd theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,7 @@ release = '0.0.1'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx_rtd_theme", 
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,7 +49,8 @@ exclude_patterns = []
 #
 # html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
-html_style = "default.css"
+html_theme_path = ["./_themes"]
+# html_style = "default.css"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
